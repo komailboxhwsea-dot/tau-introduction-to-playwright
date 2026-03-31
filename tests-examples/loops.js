83db6fa8b2dd61b30ea4fa4319d3a869d6b9a72f
Mitchell Ko <komailboxhwsea@gmail.com>
2026-03-30 22:32:18 -0700

    loops and while-loops files html js

---
+++ tests-examples/loops.js
@@ -0,0 +1,12 @@
+const names = ["Alice", "Bob", "Charlie"];
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]); // 0, 1, 2 
+}   
+
+const numbers = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i] * 2); // 1, 2, 3, 4, 5
+}           
+
---
+++ tests-examples/while-loops.js
@@ -0,0 +1,27 @@
+let password = "tommy123";
+let userGuess = "";
+
+// while (userGuess != password) {
+//     userGuess = prompt("Please enter your password:");
+// }
+
+alert("Access granted!");
+
+// 5 * 4 * 3 * 2 * 1 = 120
+let factorial = 1;
+let number = 5;
+let original = number;
+
+do {
+    factorial *= number;  // factorial = factorial * number
+    number--;             // number = number - 1
+} while (number > 0);
+
+console.log(original + " factorial is " + factorial);
+
+
+
+
+
+
+
